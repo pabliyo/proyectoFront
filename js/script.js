74775a2133e1b5c73e05c86fd4f2001336a978e3
pablo <pablofeliz1989@gmail.com>
2024-12-07 05:02:20 -0300

    cambios para el final

---
+++ js/script.js
@@ -0,0 +1,37 @@
+//Login
+/*
+const usuarioBD ="user"
+const passBD ="123456"
+
+let usuario = prompt("Ingrese su usuario:")
+let pass = prompt("Ingrese su clave:")
+
+if((usuario==usuarioBD) && (pass==passBD)){
+    alert("ingreso exitoso!")
+}else{
+    alert("el usuario o contraseña es incorrecto")
+}
+    */
+
+/*
+function saludar () {
+
+}
+
+*/
+
+//funcion on click
+
+/*
+html
+h1 id="titulo">Contenido<h1/>
+
+JS
+document.getElementById("titulo").textContent = "cambio contenido"
+*/
+
+/*
+*/
+
+/*
+*/
