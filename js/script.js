313bae66b241591d5cf548a1cc6ab47af17c7842
pablo <pablofeliz1989@gmail.com>
2024-12-07 09:38:36 -0300

    cambios para el final

--- js/script.js
+++ js/script.js
@@ -12,3 +12,86 @@ function validarForm() {
       return true;
     }
 }
+
+
+const boton = document.getElementById("botonAg")
+
+function resaltarBoton() {
+    boton.style.backgroundColor = "green";
+}
+  
+function quitarResaltado() {
+    boton.style.backgroundColor = "blue";
+}
+
+function mostrarMensaje() {
+  alert("¡Has hecho clic en el botón!");
+}
+
+function listarProductos() {
+    fetch('/proyecto/json/datos.json')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(producto => {
+            console.log(producto.nombre, producto.precio);
+        });
+  });
+}
+
+listarProductos();
+    
+function load() { // Función que establece el EventListener()
+    
+    boton.addEventListener("click", mostrarMensaje);
+    boton.addEventListener("mouseover", resaltarBoton);
+    boton.addEventListener("mouseout", quitarResaltado);
+    
+}
+
+document.addEventListener("DOMContentLoaded", load, false)
+
+
+function agregarProducto(event) {
+  var producto = {
+    id: event.target.getAttribute('id'),
+    nombre: event.target.getAttribute('nombre'),
+    precio: event.target.getAttribute('precio')
+  };
+
+  var carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+  carrito.push(producto);
+  localStorage.setItem('carrito', JSON.stringify(carrito));
+  actualizarCarrito();
+}
+
+function agregarAlCarrito(producto) {
+    // Obtener el carrito desde el LocalStorage o inicializarlo si no existe
+    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+  
+    // Agregar el producto al carrito
+    carrito.push(producto);
+  
+    // Guardar el carrito actualizado en el LocalStorage
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+  
+    // Actualizar la lista de productos en el HTML
+    actualizarListaCarrito();
+}
+  
+function actualizarListaCarrito() {
+    // Obtener el elemento donde se mostrará la lista de productos
+    const listaCarrito = document.getElementById('lista-carrito');
+  
+    // Limpiar la lista antes de agregar los nuevos productos
+    listaCarrito.innerHTML = '';
+  
+    // Obtener el carrito actualizado del LocalStorage
+    const carrito = JSON.parse(localStorage.getItem('carrito'));
+  
+    // Iterar sobre los productos del carrito y crear elementos HTML
+    carrito.forEach(producto => {
+      const li = document.createElement('li');
+      li.textContent = producto.nombre; // Asumiendo que el producto tiene una propiedad "nombre"
+      listaCarrito.appendChild(li);
+    });
+  }
